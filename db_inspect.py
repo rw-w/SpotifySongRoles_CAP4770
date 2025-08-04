1195fcbdca7f691f425dd65aae800a74aedc853d
Riley Willis <122632109+rw-w@users.noreply.github.com>
2025-08-03 20:33:42 -0400

    Final touches
    Added additional functionality to the db_inspect script. Added a theme heatmap visual, and updated the master_script to fix bugs with the staple_scores.pkl file

--- db_inspect.py
+++ db_inspect.py
@@ -25,5 +25,21 @@ def inspect_db_schema(db_file):
     except:
         print("Error inspecting the database.")
 
+def song_feature_random_from_each_cluster(n):
+    try:
+        with sqlite3.connect("dbs/song_features.db") as conn:
+            cursor = conn.cursor()
+            for cluster in range(4):
+                cursor.execute(
+                        "SELECT track_uri, track_name, artist_name, cluster, role_annotation "
+                        "FROM song_features WHERE cluster = ? ORDER BY RANDOM() LIMIT ?",
+                        (cluster, n)
+                    )
+                for column in cursor.fetchall():
+                    print(column)
+    except Exception as e:
+        print(f"Error printing from the database. Error: {e}")
+
 connection = sqlite3.connect(database_file)
-inspect_db_schema(database_file)
+#inspect_db_schema(database_file)
+song_feature_random_from_each_cluster(3)
